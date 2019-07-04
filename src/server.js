fac264e9ac920530749cfbfa47f058b7f3df5320
Nathan Carlson <nathan.s.carlson@gmail.com>
2019-07-04 13:32:40 -0600

    add new endpoints

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('This is a test!'))
+app.get('/description', (req, res) => res.send('Description'))
+app.get('/count', (req, res) => res.send('Count characters'))
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
